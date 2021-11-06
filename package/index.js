bc16ec58bc58a22ea9e985e162345abd8d69afe9
Yago <vordred7@gmail.com>
2021-11-06 18:19:12 -0300

    fetch dependency removed

--- package/index.js
+++ package/index.js
@@ -1,26 +1,36 @@
-const tpde = async () => {
-  const { default: fetch } = await import("node-fetch")
+const http = require("https")
 
-  const res = await (
-    await fetch("https://thispersondoesnotexist.com/image", {
-      headers: {
-        accept: "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
-        "sec-ch-ua": '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "image",
-        "sec-fetch-mode": "no-cors",
-        "sec-fetch-site": "same-origin",
-      },
-      referrer: "https://thispersondoesnotexist.com/",
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: null,
-      method: "GET",
-      mode: "cors",
-      credentials: "omit",
+const tpde = async () => {
+  try {
+    const res = await new Promise((resolve, reject) => {
+      const options = {
+        method: "GET",
+        hostname: "thispersondoesnotexist.com",
+        path: "/image",
+      }
+
+      const req = http.request(options, (res) => {
+        const chunks = []
+
+        res.on("data", (chunk) => {
+          chunks.push(chunk)
+        })
+
+        res.on("end", () => {
+          const body = Buffer.concat(chunks)
+          resolve(body)
+        })
+        res.on("error", (err) => {
+          reject(err)
+        })
+      })
+
+      req.end()
     })
-  ).buffer()
-  return res
+    return res
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = tpde
